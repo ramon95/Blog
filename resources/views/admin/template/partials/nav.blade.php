bb848d23c080c1b2ff9fefdf6fd8062e876abf01
Ramon <ramon.a.p.r.95@gmail.com>
2017-10-04 21:34:56 -0400

    nav
    arreglando la vista nav

--- resources/views/admin/template/partials/nav.blade.php
+++ resources/views/admin/template/partials/nav.blade.php
@@ -17,21 +17,24 @@
         <ul class="nav navbar-nav">
           <li><a href="{{ route('users.index') }}">Usuarios</a></li>
           <li><a href="{{ route('categories.index') }}">Categorias</a></li>
-          <li><a href="#">Articulos</a></li>
+          <li><a href="{{ route('articles.index') }}">Articulos</a></li>
           <li><a href="#">Imagenes</a></li>
           <li><a href="{{ route('tags.index') }}">Tags</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">Pagina Principal</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li>
-                <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Salir</a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}
-                </form>
-              </li>
-            </ul>
+          <li>
+            <a href="#">{{ Auth::user()->name }}</a>
+          </li>
+          <li>
+            <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Salir</a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}
+            </form>
+          </li>
+        </ul>
+      @else
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <a href="{{ route('login') }}">Entrar</a>
           </li>
         </ul>
       @endif
